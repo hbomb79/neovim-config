bd3fdad5e75b6ee00cb993ebcddd577d4cc0ff0c
Harry Felton <harryfelton12@gmail.com>
2023-08-24 00:16:11 +1200

    removed redundant eager-loading param for colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,7 +3,6 @@ return {
         "catppuccin/nvim",
         name = "catppuccin",
         priority = 1000,
-        config = function() vim.cmd.colorscheme "catppuccin-mocha" end,
-        lazy = false
+        config = function() vim.cmd.colorscheme "catppuccin-mocha" end
     }
 }

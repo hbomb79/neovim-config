02abd8fa640549176571e7c869a80d90c238ff1e
Harry Felton <harryfelton12@gmail.com>
2024-05-06 13:35:58 +1200

    feat: enable colorscheme inactive window dimming

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,7 +4,25 @@ return {
 		name = "catppuccin",
 		priority = 1000,
 		config = function()
-			vim.cmd.colorscheme("catppuccin-mocha")
+			require("catppuccin").setup({
+				flavour = "mocha",
+				term_colors = true,
+				dim_inactive = {
+					enabled = true,
+					shade = "dark",
+					percentage = 0.15,
+				},
+				default_integrations = true,
+				integrations = {
+					cmp = true,
+					gitsigns = true,
+					nvimtree = true,
+					treesitter = true,
+					notify = true,
+				},
+			})
+
+			vim.cmd.colorscheme("catppuccin")
 		end,
 	},
 }

0d57cb71e39bac7561716f1c57c5d3dc12da2b62
Harry Felton <harryfelton12@gmail.com>
2024-05-07 12:28:43 +1200

    feat: lazy load SQL LSP config

--- lua/lang/sql.lua
+++ lua/lang/sql.lua
@@ -1,3 +1,11 @@
 -- SQL support doesn't need a plugin, we just want to
--- ensure the LSP configuration has run.
-require("plugins.config.lsp.sql")
+-- ensure the LSP configuration has run whenever we visit a .sql
+-- file.
+vim.api.nvim_create_autocmd("BufNew", {
+	pattern = { "*.sql" },
+	once = true,
+	callback = function()
+		require("plugins.config.lsp.sql")
+		return true
+	end,
+})

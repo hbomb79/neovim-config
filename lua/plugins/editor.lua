053924f3f37ac53fd771d48765db02752fcea0ff
Harry Felton <harryfelton12@gmail.com>
2024-08-02 14:19:17 +1200

    dep: upgrade plugins

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -90,4 +90,21 @@ return {
 		dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
 		config = true,
 	},
+	{
+		"smoka7/multicursors.nvim",
+		event = "VeryLazy",
+		dependencies = {
+			"nvimtools/hydra.nvim",
+		},
+		opts = {},
+		cmd = { "MCstart", "MCvisual", "MCclear", "MCpattern", "MCvisualPattern", "MCunderCursor" },
+		keys = {
+			{
+				mode = { "v", "n" },
+				"<Leader>m",
+				"<cmd>MCstart<cr>",
+				desc = "Create a selection for selected text or word under the cursor",
+			},
+		},
+	},
 }

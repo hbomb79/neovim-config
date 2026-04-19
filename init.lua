c6d5b0852bfc2d4c063172a69408e7e558028b45
Harry Felton <harryfelton12@gmail.com>
2026-04-19 23:13:16 +1200

    fix: monkey-patch zpack apply_keys

--- init.lua
+++ init.lua
@@ -16,8 +16,28 @@ require("settings")
 -- Initialise language registry (see ./lua/langs/)
 require("langs"):setup()
 
--- Load plugins
+-- Load zpack for plugin management
 vim.pack.add({ "https://github.com/zuqini/zpack.nvim" })
+
+-- Monkey-patch zpacks 'apply_keys' function to detect keymaps with no
+-- 'RHS' and map them to <NOP> rather than just skipping them. Without
+-- this, which_key gets confused sometimes and the group loses its label.
+--
+-- Caution: This will likely break with zpack updates. Should be trivial to
+-- resolve if/when it does
+--
+---@diagnostic disable-next-line: duplicate-set-field
+require("zpack.keymap").apply_keys = function(keys)
+	local key_list = require("zpack.utils").normalize_keys(keys) --[[@as zpack.KeySpec[] ]]
+
+	for _, key in ipairs(key_list) do
+		-- Unlike the original implementation, don't SKIP keys with no/nil rhs.
+		local rhs = key[2] or "<NOP>"
+		require("zpack.keymap").map(key[1], rhs, key.remap, key.desc, key.mode, key.nowait)
+	end
+end
+
+-- Load plugins
 require("zpack").setup()
 
 -- Handles auto-updating configuration

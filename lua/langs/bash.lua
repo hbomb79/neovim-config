c6fce65687935bfe978a5feb29a7d964c6882472
Harry Felton <harryfelton12@gmail.com>
2026-04-22 11:26:38 +1200

    feat: add bash linter and formatter

--- lua/langs/bash.lua
+++ lua/langs/bash.lua
@@ -1,5 +1,9 @@
 -- Register Bash support. Auto-installs bashls.
 require("langs"):add_spec({
 	ft = { "bash", "sh" },
-	mason_auto_install = { "bash-language-server" },
+
+	-- Note the formatter (shfmt) and linter (shellcheck) both integrate
+	-- with bash-language-server. Integration with nvim-lint or conform.nvim
+	-- is not required.
+	mason_auto_install = { "shfmt", "shellcheck", "bash-language-server" },
 })
